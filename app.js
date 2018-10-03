4aea597b69d2b9b97f79a6c55272df0d792c3fea
Michael <ReifeGerhart@web.de>
2018-10-03 16:34:45 +0200

    new vue 03.10.18.16

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ new Vue({
         image: 'https://assets.t3n.sc/news/wp-content/uploads/2016/05/vue.js_2.0_frontend-framework.jpg?auto=compress%2Cformat&fit=crop&fm=jpg&h=347&ixlib=php-1.1.0&q=65&w=620&s=8398eacd030d9955f6805f64df9e66fe',
         counter: 0,
         x: 0,
-        y: 0
+        y: 0,
+        zahl: 0,
+        eingabewert: ""
     },
     methods: {
-        random: function() {
-            return Math.random();
+        zufall: function() {
+            this.zahl = Math.random();
         },
         addieren: function(){
             this.counter++;
@@ -19,7 +21,12 @@ new Vue({
         Koordinaten: function(event){
             this.x = event.clientX;
             this.y = event.clientY;
-
+        },
+        alertone: function(){
+            alert("Sie haben ENTER gedrueckt");
+        },
+        alerttwo: function(){
+            alert("Sie haben SPACE gedrueckt");
         }
     }
 });
