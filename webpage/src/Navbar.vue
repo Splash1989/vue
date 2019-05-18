446ffd39f4c63e176e15ee58243a92a8daee28fa
Michael <ReifeGerhart@web.de>
2019-05-18 11:58:39 +0200

    Beginning of developing my webpage

---
+++ webpage/src/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div id="innerNavBar">
+            <ul>
+                <li><a>About me</a></li>
+                <li><a>Projects</a></li>
+                <li><a>Contact</a></li>
+                <li><a>Games</a></li>
+                <li><a>Used technology</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        background-color: #111;
+    }
+    #innerNavBar {
+        opacity: 0.70;
+    }
+
+</style>
---
+++ webpage/src/technology.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="tecbackground">
+        <p>HALLO WELT</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "technology"
+    }
+</script>
+
+<style scoped>
+
+    #tecbackground{
+
+    }
+
+</style>
