7dd050572bc01e97eaee4461cd4107a457131fb7
Michael <ReifeGerhart@web.de>
2019-05-21 21:08:13 +0200

    added first logic for user input and data binding

--- webpage/src/Memory/Memory.vue
+++ webpage/src/Memory/Memory.vue
@@ -1,13 +1,33 @@
 <template>
     <div id="memorygame">
         <p>Eingabe</p>
-
+        <p>How many cards?</p>
+        <input id="amountofcards" placeholder="Enter number" v-model="cardamount">
+        <button :disabled="isNumberEmpty" id="startbutton" @click="rendercards">Start game</button>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Memory"
+        name: "Memory",
+
+        data () {
+            return {
+                cardamount: ''
+            }
+        },
+
+        methods: {
+            rendercards() {
+                console.warn('Hello Word', this.cardamount);
+            }
+        },
+
+        computed: {
+            isNumberEmpty () {
+                return !this.cardamount.length
+            }
+        },
     }
 </script>
 
