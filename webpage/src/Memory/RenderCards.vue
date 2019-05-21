2c58e83e13e6c2eea6f5391ee0f387a95fb0879b
Michael <ReifeGerhart@web.de>
2019-05-21 21:07:08 +0200

    new component for card rendering

---
+++ webpage/src/Memory/RenderCards.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Memory>
+        </Memory>
+    </div>
+</template>
+
+<script>
+    import Memory from "./Memory.vue";
+
+    export default {
+        name: "RenderCards",
+        components: {Memory}
+    }
+
+    components: {
+        Memory
+    }
+</script>
+
+<style scoped>
+
+</style>
