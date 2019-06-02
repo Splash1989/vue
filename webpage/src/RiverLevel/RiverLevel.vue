e4835d4d1d22619d3c9088a3e91a5889f9c3c2ad
Michael <ReifeGerhart@web.de>
2019-06-02 14:08:09 +0200

    added table for river data

--- webpage/src/RiverLevel/RiverLevel.vue
+++ webpage/src/RiverLevel/RiverLevel.vue
@@ -1,17 +1,28 @@
 <template>
     <div id="riverLevel">
-        <p>Pegelstand Elbe</p>
         <img
                 src="https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/DRESDEN/W/measurements.png?start=P15D&width=925&height=220"
                 width="925"
                 height="220"/>
-        <p>{{riverObject}}</p>
-        <div>
-            <p>timestamp</p>
-            <p>{{riverObject.data.timestamp}}</p>
-            <p>hight in cm</p>
-            <p>{{riverObject.data.value}}</p>
-        </div>
+        <!--<p>{{riverObject}}</p>-->
+        <table class="table table-hover table-dark">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">timestamp</th>
+                <th scope="col">hight in cm</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>{{riverObject.data.timestamp}}</td>
+                <td>{{riverObject.data.value}}</td>
+            </tr>
+            </tbody>
+        </table>
+
+
     </div>
 </template>
 
@@ -37,5 +48,9 @@
 <style scoped>
 
     #riverLevel {}
+
+    .table {
+        margin-top: 20px;
+    }
 
 </style>
