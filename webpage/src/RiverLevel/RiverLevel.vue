55354cf3ba3eb88da54840d60857c80a39d30fbf
Michael <ReifeGerhart@web.de>
2019-06-06 21:49:20 +0200

    dynamic graph with information from API

--- webpage/src/RiverLevel/RiverLevel.vue
+++ webpage/src/RiverLevel/RiverLevel.vue
@@ -1,7 +1,10 @@
 <template>
     <div id="riverLevel">
         <trend
-                :data="[0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]"
+                :data="[waterGauge[0], waterGauge[1], waterGauge[2], waterGauge[3],
+                 waterGauge[4], waterGauge[5], waterGauge[6], waterGauge[7], waterGauge[8],
+                  waterGauge[9], waterGauge[10], waterGauge[11], waterGauge[12], waterGauge[13],
+                   waterGauge[14]]"
                 :gradient="['#6fa8dc', '#42b983', '#2c3e50']"
                 auto-draw
                 smooth
@@ -37,6 +40,9 @@
         data () {
             return {
                 riverObject: {},
+                waterGauge: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                storage: '',
+                i: 0
             }
         },
 
@@ -45,9 +51,25 @@
             setInterval (function () {
                 axios.get("https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/DRESDEN/W/currentmeasurement.json")
                     .then(response => _this.riverObject = response)
+                _this.newPost();
             }, 5000);
             axios.get("https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/DRESDEN/W/currentmeasurement.json")
                 .then(response => this.riverObject = response)
+        },
+
+        methods: {
+            newPost() {
+                if (this.storage !== this.riverObject.data.timestamp) {
+                    this.storage = this.riverObject.data.timestamp;
+                    this.waterGauge[this.i] = this.riverObject.data.value;
+                    console.warn('HALLO HIER SPRICHT NEW POST');
+                    console.warn('WATERGAUGE- - - ', this.waterGauge);
+                    this.i++;
+                } else {
+                    console.warn('WERTE SIND GLEICH');
+                }
+            }
+
         }
     }
 </script>
