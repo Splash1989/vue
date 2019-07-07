709d61d67b07d65d118c831d28105d5c4f67916e
Michael <ReifeGerhart@web.de>
2019-07-07 17:01:10 +0200

    new component timeline

--- webpage/src/Aboutme.vue
+++ webpage/src/Aboutme.vue
@@ -3,22 +3,27 @@
         <NavBar></NavBar>
         <div>About me</div>
         <div>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+        <Timeline></Timeline>
     </div>
 </template>
 
 <script>
     import NavBar from './Navbar.vue'
+    import Timeline from'./Timeline.vue'
 
     export default {
         name: "Aboutme",
 
         components: {
-            NavBar
+            NavBar,
+            Timeline
         }
     }
 </script>
 
 <style scoped>
-    #aboutme {}
+    #aboutme {
+        background-color: white;
+    }
 
 </style>
